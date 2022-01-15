edfbefd5ff30edb9903caf432f15abf18274be91
dmike16 <cipmiky@gmail.com>
2022-01-15 22:10:48 +0100

    feat: dog vs cat model

--- intro_to_machine_learning/bag/dogs_vs_cats.py
+++ intro_to_machine_learning/bag/dogs_vs_cats.py
@@ -15,13 +15,25 @@ data set
 """
 
 import os
+import numpy as np
+import matplotlib.pyplot as plt
 from typing import Tuple
 from tensorflow import keras
+from tensorflow.keras import preprocessing
 
 _URL = 'https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip'
 
 
 class DogVSCat:
+    @staticmethod
+    def plots_images(images_arr: np.array):
+        fig, axes = plt.subplots(1, len(images_arr), figsize=(20, 20))
+        axes = axes.flatten()
+        for img, ax in zip(images_arr, axes):
+            ax.imshow(img)
+        plt.tight_layout()
+        plt.show()
+
     def __init__(self):
         self._dir_path = keras.utils.get_file('cats_and_dogs_filterted.zip', origin=_URL, extract=True)
 
@@ -33,6 +45,9 @@ class DogVSCat:
         self._train_dogs_dir = os.path.join(train_dir, 'cats')
         self._validation_cats_dir = os.path.join(validation_dir, 'cats')
         self._validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+
+        self._train_dir = train_dir
+        self._validation_dir = validation_dir
 
     def __repr__(self) -> str:
         return 'DogVSCat()'
@@ -58,6 +73,15 @@ class DogVSCat:
 
         return num_dogs_tr, num_dogs_vl
 
+    def train_image_generator_flow(self, batch_size: int, target_size: int) -> preprocessing.image.DirectoryIterator:
+        return DogVSCat._image_genrator_flow(batch_size=batch_size,
+                                             target_size=target_size,
+                                             directory=self._train_dir)
+
+    def validation_image_generator_flow(self, batch_size: int, target_size: int):
+        return DogVSCat._image_genrator_flow(batch_size=batch_size, target_size=target_size,
+                                             directory=self._validation_dir)
+
     @property
     def path(self):
         return self._dir_path
@@ -77,3 +101,14 @@ class DogVSCat:
     @property
     def dogs_validation_path(self):
         return self._validation_dogs_dir
+
+    @staticmethod
+    def _image_genrator_flow(batch_size: int, target_size: int,
+                             directory: str) -> preprocessing.image.DirectoryIterator:
+        return preprocessing.image \
+            .ImageDataGenerator(rescale=1. / 255) \
+            .flow_from_directory(directory=directory,
+                                 batch_size=batch_size,
+                                 shuffle=True,
+                                 target_size=(target_size, target_size),
+                                 class_mode='binary')
--- intro_to_machine_learning/lesson5/dogs_vs_cats_cnn.py
+++ intro_to_machine_learning/lesson5/dogs_vs_cats_cnn.py
@@ -11,8 +11,80 @@
 # -----------------------------------------------------------
 
 import bag.dogs_vs_cats as bdc
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
 
 # load the dataset
 dogs_vs_cats = bdc.DogVSCat()
 print(dogs_vs_cats)
 
+# Variable to use in building model process
+BATCH_SIZE = 100
+IMG_SIZE = 150
+
+# prepare the data
+train_data_gen = dogs_vs_cats.train_image_generator_flow(BATCH_SIZE, IMG_SIZE)
+val_data_gen = dogs_vs_cats.validation_image_generator_flow(BATCH_SIZE, IMG_SIZE)
+
+# plotting first 5 images from training
+# sample_taining_image, _ = next(train_data_gen)
+# bdc.DogVSCat.plots_images(sample_taining_image[:5])
+
+# model creation
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(32, (3, 3), activation=tf.nn.relu, input_shape=(IMG_SIZE, IMG_SIZE, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+
+    tf.keras.layers.Conv2D(64, (3, 3), activation=tf.nn.relu, ),
+    tf.keras.layers.MaxPooling2D(2, 2),
+
+    tf.keras.layers.Conv2D(128, (3, 3), activation=tf.nn.relu, ),
+    tf.keras.layers.MaxPooling2D(2, 2),
+
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation=tf.nn.relu, ),
+    tf.keras.layers.Dense(2)
+])
+
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(0.001),
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=[tf.keras.metrics.SparseCategoricalAccuracy()]
+)
+
+model.summary()
+
+# train the model
+EPOCH = 30
+total_cat_train, total_cat_val = dogs_vs_cats.cats_samples()
+total_dog_train, total_dog_val = dogs_vs_cats.dogs_samples()
+history = model.fit_generator(
+    train_data_gen,
+    steps_per_epoch=int(np.ceil((total_cat_train + total_dog_train) / float(BATCH_SIZE))),
+    epochs=EPOCH,
+    validation_data=val_data_gen,
+    validation_steps=int(np.ceil((total_cat_val + total_dog_val) / float(BATCH_SIZE))),
+)
+# view results
+acc = history.history['sparse_categorical_accuracy']
+val_acc = history.history['val_sparse_categorical_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(EPOCH)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
